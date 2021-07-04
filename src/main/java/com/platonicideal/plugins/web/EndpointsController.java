343a0c128bb99de7158b708ced3d1ec5df53d0bf
WASte <WASte@10.1.10.229>
2021-07-04 17:35:42 -0400

    Using spring classpath scanning component

--- src/main/java/com/platonicideal/plugins/web/EndpointsController.java
+++ src/main/java/com/platonicideal/plugins/web/EndpointsController.java
@@ -1,18 +1,17 @@
 package com.platonicideal.plugins.web;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,8 +27,10 @@ public class EndpointsController {
 	@ResponseBody
 	public List<EndpointDescription> getEndpoints(
 			@RequestParam(required = false, defaultValue = "") String containing) throws ClassNotFoundException, IOException, URISyntaxException {
+		Collection<Class<?>> klazzes = getClasses("com.platonicideal");
+		System.out.println(klazzes);
 		List<EndpointDescription> endpoints = 
-				getClasses("com.platonicideal").stream()
+				klazzes.stream()
 					.flatMap(klazz -> Arrays.asList(klazz.getMethods()).stream())
 					.map(m -> m.getAnnotation(GetMapping.class))
 					.filter(an -> an != null)
@@ -41,37 +42,16 @@ public class EndpointsController {
 	}
 
 	private Collection<Class<?>> getClasses(String packageName) throws ClassNotFoundException, IOException, URISyntaxException {
-		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-		String path = packageName.replace('.', '/');
-		Enumeration<URL> resources = classLoader.getResources(path);
-		List<File> dirs = new ArrayList<File>();
-		while (resources.hasMoreElements()) {
-			URL resource = resources.nextElement();
-			URI uri = new URI(resource.toString());
-			dirs.add(new File(uri.getPath()));
+		ClassPathScanningCandidateComponentProvider provider =
+			    new ClassPathScanningCandidateComponentProvider(true);
+		Set<Class<?>> klazzes = new HashSet<>();
+		String basePackage = packageName.replace(".", "/");
+		Set<BeanDefinition> beanDefinitions = provider.findCandidateComponents(basePackage);
+		for (BeanDefinition beanDefinition : beanDefinitions) {
+			String klazzName = beanDefinition.getBeanClassName();
+			klazzes.add((Class<?>) Class.forName(klazzName));
 		}
-		List<Class<?>> classes = new ArrayList<>();
-		for (File directory : dirs) {
-			classes.addAll(findClasses(directory, packageName));
-		}
-
-		return classes;
-	}
-
-	private List<Class<?>> findClasses(File directory, String packageName) throws ClassNotFoundException {
-		List<Class<?>> classes = new ArrayList<>();
-		if (!directory.exists()) {
-			return classes;
-		}
-		File[] files = directory.listFiles();
-		for (File file : files) {
-			if (file.isDirectory()) {
-				classes.addAll(findClasses(file, packageName + "." + file.getName()));
-			} else if (file.getName().endsWith(".class")) {
-				classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
-			}
-		}
-		return classes;
+		return klazzes;
 	}
 
 	private static class EndpointDescription implements Comparable<EndpointDescription> {
